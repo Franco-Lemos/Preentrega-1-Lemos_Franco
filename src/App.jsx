b24bec59fad3b1804c0a8553ef1bd45d22aac157
Franco Lemos <fran.uvs@gmail.com>
2024-06-12 08:16:21 -0300

    Adición de ruta Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from "./components/Layout";
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
-import CartContainer from "./components/CartContainer.jsx"
-import CartProvider from './context/CartProvider.jsx'; //Visual Studio se bugeó y me da un error en esta línea (Ya comprobé que no hay errores).
+import CartContainer from './components/CartContainer'
+import CartProvider from './context/CartProvider'; //Visual Studio se bugeó y me da un error en esta línea (Ya comprobé que no hay errores).
+import Checkout from './components/Checkout';
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/category/:id" element={<ItemListContainer say = {"Sección {category}"} />} />
             <Route path="/item/:id" element={<ItemDetailContainer />} />
             <Route path="/cart" element={<CartContainer/>} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
         </Layout>
       </CartProvider>
